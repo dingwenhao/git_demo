38eb256e8d5765fb76828a213d506f9644439ce2
DJX <ding_jianxiang@163.com>
2015-08-18 16:46:05 +0800

    mody

--- src/git/HelloJava.java
+++ src/git/HelloJava.java
@@ -3,6 +3,10 @@ package git;
 public class HelloJava {
 
 	public static void main(String[] args) {
+		
+		System.out.println("你好   Java");
+		System.out.println("你好   Java");
+		System.out.println("你好   Java");
 		System.out.println("你好   Java");
 		System.out.println("你好   Java");
 		System.out.println("你好   Java");
