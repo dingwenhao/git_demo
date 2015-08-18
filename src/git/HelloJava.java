b2025de80294a23b66db37341302dc1625c5e6bd
DJX <ding_jianxiang@163.com>
2015-08-18 16:40:38 +0800

    modify

--- src/git/HelloJava.java
+++ src/git/HelloJava.java
@@ -5,6 +5,7 @@ public class HelloJava {
 	public static void main(String[] args) {
 		System.out.println("你好   Java");
 		System.out.println("你好   Java");
+		System.out.println("你好   Java");
 
 	}
 
